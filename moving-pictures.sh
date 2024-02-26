fb52ae8572169ed2cf8f0a0caa0547f7c0d1ab81
colinbarcelo <cab36048@uga.edu>
2024-02-26 13:45:58 -0500

    Download metadata

--- moving-pictures.sh
+++ moving-pictures.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 wget \
-  -O "~/qiime-2-moving-pictures-tutorial/data/sample-metadata.tsv" \
+  -O "/Users/colinbarcelo/Desktop/microbiomemeeting/qiime-2-moving-pictures-tutorial/data/sample-metadata.tsv" \
   "https://data.qiime2.org/2024.2/tutorials/moving-pictures/sample_metadata.tsv"
+
+  
